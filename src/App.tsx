2e7d28a4143c895b01ea708821218682f4d70360
Wesley Wen <wwen@WS-30056-MAC.T-mobile.com>
2021-02-20 21:30:38 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+// modules
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import * as Location from 'expo-location';
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    auth().onAuthStateChanged(userState => {
+      setUser(userState);
+
+      if (isLoading) {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) return <View><Text>Loading..</Text></View>;
+
+  if (!user) {
+    return (
+      <View>
+        <Button
+
+        />
+      </View>
+    )
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Welcome {user.email}</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ src/app/login/index.tsx
@@ -0,0 +1,19 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+const Login = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Login;
